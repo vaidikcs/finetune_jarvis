4493c66e0dae066667509494d7c42062990229c6
Vaidik Nakrani <134259164+vaidikcs@users.noreply.github.com>
2024-02-02 16:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from accelerate import FullyShardedDataParallelPlugin, Accelerator
 from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
 from datasets import load_dataset
-# from models import set_status, create_session
 from huggingface_hub import create_repo
 
 fsdp_plugin = FullyShardedDataParallelPlugin(
@@ -23,41 +22,42 @@ accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
 
 import subprocess
 
-token = "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"
-command = f"huggingface-cli login --token {token}"
-
-# Run the command
-subprocess.run(command, shell=True)
-
 # -------------------
 
 def main():
+    parser = argparse.ArgumentParser(description='pass params for finetuning.')
+    parser.add_argument('--train_data', help='train data')
+    parser.add_argument('--val_data', help='val data')
+    parser.add_argument(
+        '--lora_rank', default='32', help='LoRA Rank')
+    parser.add_argument(
+        '--epochs', default='10', help='Train epochs')
+    parser.add_argument(
+        '--batch', default='16', help='train batch size')
+    parser.add_argument(
+        '--learning_rate', default=2.3e-5, help='learning rate')
+    parser.add_argument(
+        '--final_model', default='0to60ai/second', help='Path to output file')
+    parser.add_argument(
+        '--max_length', default='512', help='max length of training data.')
+    parser.add_argument(
+        '--hf_token', help='huggingface write token')
+
+    args = parser.parse_args()
+
+    token = str(args.hf_token)
+    command = f"huggingface-cli login --token {token}"
+    subprocess.run(command, shell=True)
+    
+    repo_name = args.final_model
+    create_repo(repo_name, private=True, exist_ok=True)
+    print(repo_name)
+
     try:
-        parser = argparse.ArgumentParser(description='Description of your script.')
-
-        parser.add_argument('--job_id', default=1,
-                            help='Path to input file')
-        parser.add_argument('--input', default='default_input.txt',
-                            help='Path to input file')
-        # parser.add_argument(
-        #     '--output', default='default_input.txt', help='Path to output file')
-        parser.add_argument(
-            '--final_model', default='0to60ai/second', help='Path to output file')
-        parser.add_argument(
-            '--database_url', default="mysql+pymysql://root:12345@localhost:3306/bruData?charset=utf8mb4", help='SQL url')
-        
-        args = parser.parse_args()
-
-        repo_name = args.final_model
-        create_repo(repo_name, private=True, exist_ok=True)
-        print(repo_name)
-        # create_session(args['database_url'])
-        # set_status(args['job_id'], 'base model loading started')
-
         base_model_id = "SkunkworksAI/phi-2"
         model = AutoModelForCausalLM.from_pretrained(
             base_model_id, token=token, trust_remote_code=True, torch_dtype=torch.float16, load_in_8bit=True)
-
+    
         tokenizer = AutoTokenizer.from_pretrained(
             base_model_id,
             padding_side="left",
@@ -67,9 +67,17 @@ def main():
             use_fast=False,  # needed for now, should be fixed soon
         )
         tokenizer.pad_token = tokenizer.eos_token
-        # set_status(args['job_id'], 'dataset loading started')
-        # -------------------
-
+     except Exception as e:
+        raise Exception(f"something went wrong while downloading base model. {e}")
+    # set_status(args['job_id'], 'dataset loading started')
+    # -------------------
+    try:
+        train_data = str(arg.train_data)
+        val_data = str(arg.val_data)
+        train_dataset = load_dataset("csv", data_files=train_data, split="train")
+        eval_dataset = load_dataset("csv", data_files=val_data, split="train")
+    except Exception as e:
+        print(e)
         data = ['Instruct: Create a new experiment named "MLflowDemo" with a custom artifact location..\nOutput: <<<api_call>>>: 2.0/mlflow/experiments/create\n<<<api_call_type>>>: POST\n<<<params>>>: {\n    "name": "MLflowDemo",\n    "artifact_location": "/custom/location"\n}\n<<<explanation>>>: This API call creates a new MLflow experiment named "MLflowDemo" with a custom artifact location set to "/custom/location".. <s>',
                 'Instruct: Find MLflow experiments with a maximum of 5 results, filtered by name \'MLflowDemo\', ordered by start time, and showing only active experiments..\nOutput: <<<api_call>>>: 2.0/mlflow/experiments/search\n<<<api_call_type>>>: POST\n<<<params>>>: {"max_results": 5, "filter": "name=\'MLflowDemo\'", "order_by": ["start_time"], "view_type": "ACTIVE"}\n<<<explanation>>>: Use this API to search for MLflow experiments with a maximum of 5 results, filtered by name \'MLflowDemo\', ordered by start time, and showing only active experiments.. <s>',
                 'Instruct: Fetch information about the MLflow experiment with ID 123..\nOutput: <<<api_call>>>: 2.0/mlflow/experiments/get\n<<<api_call_type>>>: GET\n<<<params>>>: {"experiment_id": "123"}\n<<<explanation>>>: Retrieve details of the MLflow experiment with ID 123 using this API call.. <s>',
@@ -90,12 +98,14 @@ def main():
                 'Instruct: As a project lead, update the description of the registered model with the name "CustomerSegmentation" to "Updated model for segmenting customer data"..\nOutput: <<<api_call>>>: 2.0/mlflow/registered-models/update\n<<<api_call_type>>>: PATCH\n<<<params>>>: {"name": "CustomerSegmentation", "description": "Updated model for segmenting customer data"}\n<<<explanation>>>: Project leads can use this API to update the description of the registered model with the name "CustomerSegmentation" to "Updated model for segmenting customer data".. <s>',
                 'Instruct: As a model administrator, delete the registered model with the name "LegacyModel"..\nOutput: <<<api_call>>>: 2.0/mlflow/registered-models/delete\n<<<api_call_type>>>: DELETE\n<<<params>>>: {"name": "LegacyModel"}\n<<<explanation>>>: Model administrators can use this API to delete the registered model with the name "LegacyModel".. <s>',
                 'Instruct: As a data scientist, search for model versions based on a filter condition and order the results by the creation timestamp..\nOutput: <<<api_call>>>: 2.0/mlflow/model-versions/search\n<<<api_call_type>>>: GET\n<<<params>>>: {"filter": "name LIKE \'image-classifier\'", "max_results": 50, "order_by": ["creation_timestamp"]}\n<<<explanation>>>: Data scientists can use this API to search for model versions with a filter condition, like \'name LIKE \'image-classifier\'\', order the results by the creation timestamp, and limit to a maximum of 50 results.. <s>']
-
+    
         train_dataset = Dataset.from_dict({'text': data[:14]})
         eval_dataset = Dataset.from_dict({'text': data[14:]})
 
-        max_length = 512  # This was an appropriate max length for my dataset
-
+      # This was an appropriate max length for my dataset
+    try:
+        max_length = int(arg.max_length)
+        
         def generate_and_tokenize_prompt2(prompt):
             result = tokenizer(
                 prompt['text'],
@@ -105,52 +115,50 @@ def main():
             )
             result["labels"] = result["input_ids"].copy()
             return result
-
-
+    
+    
         tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt2)
         tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt2)
+    except Exception as e:
+        raise Exception(f"something went wrong while parsing train data. {e}")
 
-        # -----------------
-
-        # set_status(args.job_id, 'configuring lora settings')
-
+    # -----------------
+    rank = int(arg.lora_rank)
+    try:
         config = LoraConfig(
-            r=32,
-            lora_alpha=64,
+            r=rank,
+            lora_alpha=2*rank,
             target_modules=[
                 'Wqkv',
                 'out_proj',
                 "fc1",
                 "fc2"
             ],
-            # target_modules=[
-            #     "Wqkv",
-            #     "fc1",
-            #     "fc2",
-            # ],
             bias="none",
             lora_dropout=0.1,  # Conventional
             task_type="CAUSAL_LM",
         )
         model.config.gradient_checkpointing = False
         model = get_peft_model(model, config)
-        # print_trainable_parameters(model)
+        model = accelerator.prepare_model(model)  
+    except Exception as e:
+        raise Exception(f"something went wrong while setting up LoRA. {e}")
+    # print_trainable_parameters(model)
 
-        # -------------------
+    # ------------------
+    if torch.cuda.device_count() > 1:  # If more than 1 GPU
+        model.is_parallelizable = True
+        model.model_parallel = True
+    project = "best"
+    base_model_name = "phi2"
+    run_name = base_model_name + "-" + project
+    output_dir = "./" + run_name
 
-        model = accelerator.prepare_model(model)
-
-        # -------
-        # set_status(args['job_id'], 'training started')
-
-        if torch.cuda.device_count() > 1:  # If more than 1 GPU
-            model.is_parallelizable = True
-            model.model_parallel = True
-        project = "best"
-        base_model_name = "phi2"
-        run_name = base_model_name + "-" + project
-        output_dir = "./" + run_name
-
+    try:
+        epochs = int(arg.epochs)
+        batch = int(arg.batch)
+        learning_rate = float(arg.learning_rate)
+        
         trainer = transformers.Trainer(
             model=model,
             train_dataset=tokenized_train_dataset,
@@ -158,10 +166,10 @@ def main():
             args=transformers.TrainingArguments(
                 output_dir=output_dir,
                 warmup_steps=1,
-                per_device_train_batch_size=2,
+                per_device_train_batch_size=batch,
                 gradient_accumulation_steps=3,
-                num_train_epochs=20,
-                learning_rate=2.3e-5,  # Want a small lr for finetuning
+                num_train_epochs=epochs,
+                learning_rate=learning_rate,  # Want a small lr for finetuning
                 optim="paged_adamw_8bit",
                 logging_steps=25,              # When to start reporting loss
                 logging_dir="./logs",        # Directory for storing logs
@@ -177,12 +185,15 @@ def main():
             data_collator=transformers.DataCollatorForLanguageModeling(
                 tokenizer, mlm=False),
         )
-
+    
         # silence the warnings. Please re-enable for inference!
         model.config.use_cache = False
         trainer.train()
+    except Exception as e:
+        raise Exception(f"something went wrong during training. {e}")
 
-        # set_status(args['job_id'], 'saving model')
+    # set_status(args['job_id'], 'saving model')
+    try:
         base_model_id = "SkunkworksAI/phi-2"#"microsoft/phi-2"
         base_model = AutoModelForCausalLM.from_pretrained(
             base_model_id,  # Phi2, same as before
@@ -195,7 +206,7 @@ def main():
         
         eval_tokenizer = AutoTokenizer.from_pretrained(base_model_id, add_bos_token=True, trust_remote_code=True, use_fast=False)
         eval_tokenizer.pad_token = eval_tokenizer.eos_token
-
+    
         del trainer, model
         from peft import PeftModel
         path = os.listdir(run_name)[-1]
@@ -206,11 +217,9 @@ def main():
         print('****')
         eval_tokenizer.push_to_hub(repo_id = repo_name)
         print('finished')
-        # set_status(args['job_id'], 'training finished')
-        
     except Exception as e:
-        print(e)
-        # set_status(args['job_id'], f'error : {e[:200]}')
+        raise Exception(f"something went wrong while saving model. {e}")
+        
 
 if __name__ == '__main__':
     main()
