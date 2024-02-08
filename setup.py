c96b58cbb6c8f0a8468f4bb1996b0e0db7f60d8a
vaidik nakrani <vaidik.nakrani@cloudseed.io>
2024-02-08 12:08:06 +0530

    f

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
 import os
 import subprocess
-import shutil
 import urllib.request
+
 
 def download_anaconda_installer():
     url = "https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh"
     filename = "Anaconda3-2021.05-Linux-x86_64.sh"
     urllib.request.urlretrieve(url, filename)
 
+
 def install_anaconda():
-    subprocess.run(["sudo", "bash", "Anaconda3-2021.05-Linux-x86_64.sh", "-b", "-p", "/opt/conda"])
+    subprocess.run(
+        ["sudo", "bash", "Anaconda3-2021.05-Linux-x86_64.sh", "-b", "-p", "/opt/conda"])
     subprocess.run(["/opt/conda/bin/conda", "init"])
     subprocess.run(["exec", "$SHELL"])
 
+
 def create_python_env():
-    subprocess.run(["/opt/conda/bin/conda", "create", "--prefix", "/opt/conda/envs/python39", "python=3.9", "-y"])
-    subprocess.run(["/opt/conda/bin/conda", "activate", "/opt/conda/envs/python39"])
+    # Activate the conda base environment
+    os.environ["PATH"] = "/opt/conda/bin:" + os.environ["PATH"]
+
+    # Create the python39 environment
+    subprocess.run(["conda", "create", "--prefix",
+                   "/opt/conda/envs/python39", "python=3.9", "-y"])
+    subprocess.run(["conda", "init", "bash"])
+    subprocess.run(["exec", "$SHELL"])
+
 
 def clone_repository():
     subprocess.run(["git", "clone", "https://github.com/vaidikcs/test.git"])
+
 
 def main():
     if not shutil.which("conda"):
@@ -29,6 +40,7 @@ def main():
         os.makedirs("/opt/conda/envs")
 
     create_python_env()
+
 
 if __name__ == "__main__":
     main()
