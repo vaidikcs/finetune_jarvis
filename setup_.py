96bfd57d733b03966a4e9bb8ddcd60038678c4f5
vaidik nakrani <vaidik.nakrani@cloudseed.io>
2024-02-09 16:20:43 +0530

    s

--- setup_.py
+++ setup_.py
@@ -1,46 +1,24 @@
+import os
 import subprocess
 import sys
-import venv
 
 
-def create_python_env(env_path):
-    """
-    Create a new Python environment with the specified Python version using venv.
-    
-    Args:
-    - env_path (str): Path where the environment should be created.
-    - python_version (str): Version of Python for the environment.
-    """
-    subprocess.run(["python3.9", "-m", "venv", env_path], check=True)
-
-
-
-def activate_python_env(env_path):
-    """
-    Activate a Python environment.
-    
-    Args:
-    - env_path (str): Path to the environment to activate.
-    """
-    if sys.platform == 'win32':
-        activate_script = "Scripts/activate.bat"
-    else:
-        activate_script = "bin/activate"
-
-    activate_path = f"{env_path}/{activate_script}"
-    command = f"source {activate_path}"
-    subprocess.run(command, shell=True, check=True)
+def create_venv(venv_dir):
+    try:
+        subprocess.check_output([sys.executable, '-m', 'venv', venv_dir])
+        print(f"Virtual environment created successfully at {venv_dir}")
+    except subprocess.CalledProcessError:
+        print("Error occurred while creating the virtual environment.")
+        sys.exit(1)
 
 
 def main():
-    env_path = "/home/myenv"
-    python_version = "3.9"
+    # if len(sys.argv) != 2:
+    #     print("Usage: python create_venv.py <venv_directory>")
+    #     sys.exit(1)
 
-    # Create the Python environment
-    create_python_env(env_path)
-
-    # Activate the Python environment
-    # activate_python_env(env_path)
+    venv_dir = "/home/myenv"
+    create_venv(venv_dir)
 
 
 if __name__ == "__main__":
