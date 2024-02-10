cd757ad13f6dd3efbbeb9469ad22cfaa4368fcde
vaidik nakrani <vaidik.nakrani@cloudseed.io>
2024-02-10 14:31:14 +0530

    d

--- setup_.py
+++ setup_.py
@@ -3,9 +3,9 @@ import subprocess
 import sys
 
 
-def create_venv(venv_dir):
+def create_venv(venv_dir, python_executable):
     try:
-        subprocess.check_output([sys.executable, '-m', 'venv', venv_dir])
+        subprocess.check_output([python_executable, '-m', 'venv', venv_dir])
         print(f"Virtual environment created successfully at {venv_dir}")
     except subprocess.CalledProcessError:
         print("Error occurred while creating the virtual environment.")
@@ -13,12 +13,15 @@ def create_venv(venv_dir):
 
 
 def main():
+    # Path to the Python 3.9 interpreter
+    python_3_9_executable = "/usr/bin/python3.9"
+
     # if len(sys.argv) != 2:
     #     print("Usage: python create_venv.py <venv_directory>")
     #     sys.exit(1)
 
     venv_dir = "/home/myenv"
-    create_venv(venv_dir)
+    create_venv(venv_dir, python_3_9_executable)
 
 
 if __name__ == "__main__":
